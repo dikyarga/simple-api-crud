acc885b986843b6073cfe38a2c409e65db09b3c2
Diky Arga <dikyarga.id@gmail.com>
2017-03-09 10:36:28 +0700

    create foodController

---
+++ controllers/foodController.js
@@ -0,0 +1,57 @@
+let Food = require('../models/food')
+
+module.exports = {
+  index: function(req, res, next){
+    Food.find({}, function(err, foods){
+      if (err) {
+        res.send(err)
+      } else {
+        res.json(foods)
+      }
+    })
+  },
+  create: function(req, res, next){
+    let food = new Food({
+      name: req.body.name,
+      price: req.body.price,
+      expired_date : req.body.expired_date
+    })
+
+    food.save(function(err){
+      if(err){
+        res.send(err)
+      } else {
+        res.send('Food saved')
+      }
+    })
+  },
+  show: function(req, res, next){
+    Food.findById(req.params.id, function(err, food){
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(food)
+      }
+    })
+  },
+  destroy: function(req, res, next){
+    // Using find and remove
+    Food.findByIdAndRemove(req.params.id, function(err){
+      if(err) throw err
+      res.json('Food Deleted')
+    })
+  },
+  update: function(req, res, next){
+    Food.findById(req.params.id, function(err, food){
+      if(err) throw err
+      food.name = req.body.name
+      food.price = req.body.price
+      fodd.expired_date = req.body.expired_date
+
+      food.save(function(err){
+        if(err) throw err
+        res.json('Food updated')
+      })
+    })
+  }
+}
